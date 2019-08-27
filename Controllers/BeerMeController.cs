3a784a6981d2e39c62e3fb07ee0727e421956177
Andrew Coleman <Andrew@lawn-128-61-127-169.lawn.gatech.edu>
2019-08-26 20:32:01 -0400

    initial commit

---
+++ Controllers/BeerMeController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using beermeAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace beermeAPI.Controllers
+{
+    [Route("api")]
+    public class BeerMeController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public List<Recipe> Get()
+        {
+            return new List<Recipe>() {
+                new Recipe()
+                {
+                    RecipeId = 1,
+                    Name = "IPA"
+                },
+                new Recipe()
+                {
+                    RecipeId = 2,
+                    Name = "Golden Ale"
+                },
+                new Recipe()
+                {
+                    RecipeId = 3,
+                    Name = "Orange Belgian Wheat"
+                }
+            };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public Recipe Get(int id)
+        {
+            return new Recipe() {
+                RecipeId = 1,
+                Name = "IPA",
+                Ingredients = new List<Ingredient>() {
+                    new Ingredient(){
+                        IngredientId = 1,
+                        Description = "Malted Hops",
+                        Quantity = "1 oz"
+                    },
+                    new Ingredient(){
+                        IngredientId = 2,
+                        Description = "Some other hops",
+                        Quantity = "1 oz"
+                    },
+                    new Ingredient(){
+                        IngredientId = 3,
+                        Description = "Even more hops",
+                        Quantity = "1 oz"
+                    }
+                }
+            };
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/BeerMe.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+namespace beermeAPI.Models
+{
+    public class BeerMeContext : DbContext
+    {
+        public DbSet<Recipe> Recipes { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=BeerMe;Integrated Security=True");
+        }
+    }
+
+    public class Recipe
+    {
+        public int RecipeId { get; set; }
+        public string Name { get; set; }
+        public List<Ingredient> Ingredients { get; set; }
+    }
+
+    public class Ingredient
+    {
+        public int IngredientId { get; set; }
+        public string Quantity { get; set; }
+        public string Description { get; set; }
+    }
+}
